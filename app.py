671e169faea4dc562d4fd5f5828609b0a5950b9c
angelapzpa <angelapalestina612@gmail.com>
2022-11-22 20:32:57 -0600

    1.9 Funcion en app diccionario

--- app.py
+++ app.py
@@ -2,21 +2,58 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+######### Rutas Public ###########
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('public/index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('public/about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('public/contact.html')
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    projects = [
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url':'https://www.google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url':'https://www.xataka.com'
+        },
+                {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url':'https://www.google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url':'https://www.xataka.com'
+        },
+    ]
+    return render_template('public/portfolio.html',projects=projects)
+
+######### Rutas #########
+@app.route('/auth/login')
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/auth/register')
+def register():
+    return render_template('auth/register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
